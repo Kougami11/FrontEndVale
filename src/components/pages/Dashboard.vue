f45eeadd7b6d556a6bb24aec15b89b6f99e0b0e0
Kougami11 <reinaldovip@gmail.com>
2024-07-02 23:11:55 -0400

    Modificaciones de rutas y pruebas en el front.

---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Template  from '@/shared/Template.vue';
+</script>
+<template>
+<Template></Template>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Login from '../components/pages/Login.vue'
+import Dashboard from '../components/pages/Dashboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -8,6 +9,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
     },
     {
       path: '/about',
