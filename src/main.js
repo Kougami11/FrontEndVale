144676da3352a82202415d1eca92de87b8b88ffc
Kougami11 <reinaldovip@gmail.com>
2024-07-03 17:54:00 -0400

    Adding axios,nprogress y store to main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'nprogress/nprogress.css'
 import './assets/main.css'
+import axios from 'axios'
+import Constants from './Constants';
+import NProgress from 'nprogress'
+import store from "@/store";
+
+
+axios.defaults.baseURL = Constants.apiUrl // Default URL for Axios
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
-
-app.use(router)
+  
+app.use(router).use(store)
+router.beforeEach((To,From, next) => {
+    NProgress.start()       
+    next()    
+ })     
+ router.afterEach(() => {  
+    NProgress.done()    
+ }) 
 
 app.mount('#app')
 
