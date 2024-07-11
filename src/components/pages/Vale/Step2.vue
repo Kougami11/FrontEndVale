2ef323a26b53e984036f4a453b57b834f6e2e512
Kougami11 <reinaldovip@gmail.com>
2024-07-11 17:53:14 -0400

    Paso 2 del formulario.

---
+++ src/components/pages/Vale/Step2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tab-pane fade" id="step2" role="tabpanel" aria-labelledby="profile-tab">
+    <form class="form-horizontal p-3">
+      <div class="mb-3">
+        <label for="amount" class="form-label">Monto:</label>
+        <input
+          type="text"
+          class="form-control"
+          id="amount"
+          placeholder="Ingrese el monto a descontar"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">Documento PDF: </label>
+        <input class="form-control" type="file" id="formFile" />
+      </div>
+    </form>
+  </div>
+</template>
